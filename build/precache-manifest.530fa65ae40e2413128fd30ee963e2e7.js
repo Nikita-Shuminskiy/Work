f5966cbfbf279bb25dd6b51356b08003c8bbe888
Nick <shuminskiy.nik@mail.ru>
2021-08-04 00:06:35 +0300

    Home-Work 9-11

---
+++ build/precache-manifest.530fa65ae40e2413128fd30ee963e2e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9dac7d7ab14840b0ff6228a0dca3048",
+    "url": "/Work/index.html"
+  },
+  {
+    "revision": "6348a2d1abe971560492",
+    "url": "/Work/static/css/main.79552b45.chunk.css"
+  },
+  {
+    "revision": "f9fb43b0888b0a96aae9",
+    "url": "/Work/static/js/2.45c26ab0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Work/static/js/2.45c26ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6348a2d1abe971560492",
+    "url": "/Work/static/js/main.0fa1929b.chunk.js"
+  },
+  {
+    "revision": "2f3ae5999fd0bdc79df4",
+    "url": "/Work/static/js/runtime-main.788b1831.js"
+  }
+]);
